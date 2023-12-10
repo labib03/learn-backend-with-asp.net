ca0d09c2079723796f4a469ef94bfdacf8a0a2e2
labib_dev <labib.ansorudin@ptgdi.com>
2023-12-11 00:12:05 +0700

    feat: setup and connect to local server database

---
+++ DB/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace latihan.api;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Users> Users => Set<Users>();
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 using latihan.api;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
